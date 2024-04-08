80f4a049a18460efca08e26a7695e5091edd71ef
FiorellaSF <afiosf@gmail.com>
2024-04-08 10:16:54 +0200

    Fixing things

--- front/src/App.js
+++ front/src/App.js
@@ -3,8 +3,6 @@ import About from './components/About/about.jsx';
 import Tools from './components/Tools/tools.jsx';
 import Welcome from './components/Welcome/welcome.jsx';
 
-
-
 function App() {
   return (
     <main>
--- front/src/components/Welcome/welcome.jsx
+++ front/src/components/Welcome/welcome.jsx
@@ -42,8 +42,8 @@ const Welcome = () => {
         </div>
       </section> 
       <div className='buttons'>
-      <a href='https://es.linkedin.com/in/fiorella-sandoval-franco-92541b2a2' class="button-57" role="button"><span class="text">Linkedin</span><span>Vamos allá!</span></a>
-      <a href='https://github.com/FiorellaSF' class="button-57" role="button"><span class="text">GitHub</span><span>Vamos allá!</span></a>
+      <a href='https://es.linkedin.com/in/fiorella-sandoval-franco-92541b2a2' class="button-57" role="button"><span class="text">Linkedin</span><span>Visítame!</span></a>
+      <a href='https://github.com/FiorellaSF' class="button-57" role="button"><span class="text">GitHub</span><span>Visítame!</span></a>
         </div>
     </main>
   );
